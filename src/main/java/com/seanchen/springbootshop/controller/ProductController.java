9616f1f311192726adf27504371425dd829f8f8b
Sean <abbb123p@gmail.com>
2023-08-23 23:44:15 +0800

    設定分頁搜尋

--- src/main/java/com/seanchen/springbootshop/controller/ProductController.java
+++ src/main/java/com/seanchen/springbootshop/controller/ProductController.java
@@ -8,17 +8,20 @@ import com.seanchen.springbootshop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.List;
 
+@Validated
 @RestController
 public class ProductController {
 
     @Autowired
     private ProductService productService;
-
 
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getProducts(
@@ -27,7 +30,11 @@ public class ProductController {
             @RequestParam(required = false) String search,
             // 排序
             @RequestParam(defaultValue = "created_date") String orderBy,
-            @RequestParam(defaultValue = "desc") String sort
+            @RequestParam(defaultValue = "desc") String sort,
+
+            @RequestParam(defaultValue = "5") @Max(1000) @Min(0) Integer limit,
+            @RequestParam(defaultValue = "0") @Min(0) Integer offset
+
 
     ) {
         ProductQueryParams productQueryParams = new ProductQueryParams();
@@ -35,6 +42,8 @@ public class ProductController {
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
         List<Product> productList = productService.getProducts(productQueryParams);
 
